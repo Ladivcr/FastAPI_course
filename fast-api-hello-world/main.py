c1d941297655952440efc6e8dfc02a258e993aa6
Ladivcr <vrosas832@gmail.com>
2021-10-30 17:12:09 -0500

    validations_path

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi import Body
 from fastapi import Query
+from fastapi import Path
 # Instancia de FASTAPI
 app = FastAPI() # Esta variable va a contener a toda nuestra aplicación 
 
@@ -34,7 +35,34 @@ def create_person(person: Person = Body(...)): # El triple punto indica que es o
 # Validaciones: Query Parameters
 @app.get("/person/detail")
 def show_person(
-        name: Optional[str] = Query(None, min_length=1, max_length=50),
-        age: int = Query(...) # Query parameter obligatorio
+        name: Optional[str] = Query(
+            None, min_length=1,
+            max_length=50,
+            title = "Person Name", 
+            description="This is the person name. It's between 1 and 50 characters"
+            ),
+
+        age: int = Query(
+            ...,
+            title="Person Age",
+            description="This is the person age. It's required"
+            ) # Query parameter obligatorio
         ):
     return {name: age}
+
+# Validaciones: Path Parameters
+
+#TODO: Si tenemos dos enpoints que son iguales, python va a tomar el primero dado que lee de izquierda a derecha
+# y de arriba hacia abajao linea por linea
+@app.get("/person/detail/{person_id}")
+def show_person(
+        person_id: int = Path(
+            ...,
+            gt=0,
+            title="Person Id", 
+            description="This is the person Id. It's greater than zero and it's requiered"
+            )
+
+        ):
+    return {person_id: "It exists!"}
+
